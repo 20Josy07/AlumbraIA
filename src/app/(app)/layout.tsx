c4069d852a5113ce8a64ee63570aba317cb90e48
Josimar Acosta <josyacosta07@gmail.com>
2025-05-14 14:40:08 -0500

    Update layout.tsx

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,3 +3,4 @@ import AppLayout from '@/components/layout/app-layout';
 
 export default function MainAppLayout({ children }: { children: ReactNode }) {
   return <AppLayout>{children}</AppLayout>;
+}
